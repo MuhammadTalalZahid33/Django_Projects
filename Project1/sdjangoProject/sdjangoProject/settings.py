b1c34fa5d2acf667823449850dfc79227445c1f8
Muhammad Talal Zahid <talalzahid912@gmail.com>
2025-10-27 23:08:18 +0500

    installed tailwind...

--- Project1/sdjangoProject/sdjangoProject/settings.py
+++ Project1/sdjangoProject/sdjangoProject/settings.py
@@ -128,4 +128,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-NPM_BIN_PATH = r" C:\nvm4w\nodejs\npm.ps1"
+NPM_BIN_PATH = r"C:\Users\Unknown\AppData\Local\nvm\v22.15.0\npm.cmd"
